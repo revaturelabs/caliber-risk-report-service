e92672b8fb61d42b69deef9454b7f767ba0b79a4
Jimmy Duffy <jeduffy3@gmail.com>
2018-09-05 12:52:56 -0500

    Added Annotation to ServiceLayer

--- CaliberRiskReportService/src/main/java/com/revature/service/BatchWeeklyReportServiceLayer.java
+++ CaliberRiskReportService/src/main/java/com/revature/service/BatchWeeklyReportServiceLayer.java
@@ -11,11 +11,13 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author James
  */
+@Service
 public class BatchWeeklyReportServiceLayer {
     
     @Autowired
