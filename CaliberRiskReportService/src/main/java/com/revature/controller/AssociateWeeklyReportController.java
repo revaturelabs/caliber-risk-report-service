fbbd3a118bb88efa145a62df4383eb4fe3d5c6e6
calahown <blind.noldor@gmail.com>
2018-09-10 15:52:55 -0500

    Added Associate endpoint

---
+++ CaliberRiskReportService/src/main/java/com/revature/controller/AssociateWeeklyReportController.java
@@ -0,0 +1,37 @@
+package com.revature.controller;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.beans.AssociateWeeklyReport;
+import com.revature.service.AssociateWeeklyReportServiceLayer;
+
+@CrossOrigin
+@RestController
+public class AssociateWeeklyReportController {
+	@Autowired
+	private AssociateWeeklyReportServiceLayer awrsl;
+
+	public AssociateWeeklyReportController(AssociateWeeklyReportServiceLayer awrsl) {
+		this.awrsl = awrsl;
+	}
+
+	@GetMapping(value="/associates")
+	public ResponseEntity<List<AssociateWeeklyReport>> getAll() {
+		List<AssociateWeeklyReport> reports = awrsl.getAll();
+		return ResponseEntity.ok(reports);
+	}
+	
+	@GetMapping(value="/associates/{id}")
+	public ResponseEntity<List<AssociateWeeklyReport>> getAllbyId(@PathVariable("id") UUID uuid) {
+		List<AssociateWeeklyReport> reports = awrsl.getAllbyId(uuid);
+		return ResponseEntity.ok(reports);
+	}
+}
---
+++ CaliberRiskReportService/src/main/java/com/revature/service/AssociateWeeklyReportServiceLayer.java
@@ -0,0 +1,33 @@
+package com.revature.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.revature.beans.AssociateWeeklyReport;
+import com.revature.repository.AssociateWeeklyReportRepository;
+
+@Service
+public class AssociateWeeklyReportServiceLayer {
+	
+	@Autowired
+	private AssociateWeeklyReportRepository awrr;
+
+	public AssociateWeeklyReportServiceLayer(AssociateWeeklyReportRepository awrr) {
+		this.awrr = awrr;
+	}
+
+	public void setAwrr(AssociateWeeklyReportRepository awrr) {
+		this.awrr = awrr;
+	}
+	
+	public List<AssociateWeeklyReport> getAll() {
+		return awrr.findAll();
+	}
+	
+	public List<AssociateWeeklyReport> getAllbyId(UUID uuid) {
+		return awrr.findByassId(uuid);
+	}
+}
