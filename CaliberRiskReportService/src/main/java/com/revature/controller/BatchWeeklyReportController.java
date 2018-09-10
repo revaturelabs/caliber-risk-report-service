031de587c70dd8eeb4f702dc80ad2de9d67e875a
calahown <blind.noldor@gmail.com>
2018-09-10 09:45:37 -0500

    Cross origin added

--- CaliberRiskReportService/src/main/java/com/revature/controller/BatchWeeklyReportController.java
+++ CaliberRiskReportService/src/main/java/com/revature/controller/BatchWeeklyReportController.java
@@ -10,6 +10,7 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,8 @@ import com.revature.service.BatchWeeklyReportServiceLayer;
  *
  * @author James
  */
+
+@CrossOrigin
 @RestController
 public class BatchWeeklyReportController {
     
