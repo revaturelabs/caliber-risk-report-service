067f0b491f9d2098b3b7a27105d080940b3a5f63
KyleNewt <kyle.newton.1992@gmail.com>
2018-09-10 10:24:16 -0500

    Ember and Java

---
+++ CaliberRiskReportService/src/main/java/com/revature/beans/Assessment.java
@@ -0,0 +1,81 @@
+package com.revature.beans;
+
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+@JsonRootName(value="assessment")
+public class Assessment {
+	
+	private UUID assessmentId;
+	
+	private int score;
+	
+	private String title;
+	
+	private String type;
+	
+	private int week;
+	
+	private int batchId;
+	
+	private int category;
+
+	public UUID getAssessmentId() {
+		return assessmentId;
+	}
+
+	public void setAssessmentId(UUID assessmentId) {
+		this.assessmentId = assessmentId;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getWeek() {
+		return week;
+	}
+
+	public void setWeek(int week) {
+		this.week = week;
+	}
+
+	public int getBatchId() {
+		return batchId;
+	}
+
+	public void setBatchId(int batchId) {
+		this.batchId = batchId;
+	}
+
+	public int getCategory() {
+		return category;
+	}
+
+	public void setCategory(int category) {
+		this.category = category;
+	}
+	
+	
+}
---
+++ CaliberRiskReportService/src/main/java/com/revature/beans/Category.java
@@ -0,0 +1,41 @@
+package com.revature.beans;
+
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+@JsonRootName(value="category")
+public class Category {
+	
+	private UUID categoryId;
+	
+	private String skillCategory;
+	
+	private boolean active;
+
+	public UUID getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(UUID categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getSkillCategory() {
+		return skillCategory;
+	}
+
+	public void setSkillCategory(String skillCategory) {
+		this.skillCategory = skillCategory;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	
+	
+}
