e7b6a4a4f14077283517ac6b5f364a2340bc9239
TimHarrell <mrmath1618@gmail.com>
2018-09-11 09:54:08 -0500

    a

--- CaliberRiskReportService/src/test/java/com/revature/QcReportApplicationTests.java
+++ CaliberRiskReportService/src/test/java/com/revature/QcReportApplicationTests.java
@@ -1,16 +1,27 @@
 package com.revature;
 
+import org.cassandraunit.CassandraCQLUnit;
+import org.cassandraunit.dataset.cql.ClassPathCQLDataSet;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.datastax.driver.core.ResultSet;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class QcReportApplicationTests {
 
+	@Rule
+	public CassandraCQLUnit ccu = new CassandraCQLUnit(new ClassPathCQLDataSet("tables.cql","keyspaceNameToCreate"));
+	
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		
+        ResultSet result = cassandraCQLUnit.session.execute("select * from mytable WHERE id='myKey01'");
+        assertThat(result.iterator().next().getString("value"), is("myValue01"));
 	}
 
 }
