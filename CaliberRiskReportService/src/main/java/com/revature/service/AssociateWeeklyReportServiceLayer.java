b5a54a23b9dba54d89895ef6e9bde32ad5a83c8b
williamelliotscott <william_scott777@yahoo.com>
2018-09-12 10:21:51 -0500

    returns associates with too many reds

--- CaliberRiskReportService/src/main/java/com/revature/service/AssociateWeeklyReportServiceLayer.java
+++ CaliberRiskReportService/src/main/java/com/revature/service/AssociateWeeklyReportServiceLayer.java
@@ -1,8 +1,8 @@
 package com.revature.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +11,7 @@ import com.revature.repository.AssociateWeeklyReportRepository;
 
 @Service
 public class AssociateWeeklyReportServiceLayer {
-	
+
 	@Autowired
 	private AssociateWeeklyReportRepository awrr;
 
@@ -22,12 +22,44 @@ public class AssociateWeeklyReportServiceLayer {
 	public void setAwrr(AssociateWeeklyReportRepository awrr) {
 		this.awrr = awrr;
 	}
-	
+
 	public List<AssociateWeeklyReport> getAll() {
 		return awrr.findAll();
 	}
-	
+
 	public List<AssociateWeeklyReport> getAllbyId(Integer id) {
 		return awrr.findByassId(id);
+	}
+
+	public List<AssociateWeeklyReport> getReportsWithToManyReds() {
+		List<AssociateWeeklyReport> result = new ArrayList<>();
+		HashMap<Integer, List<AssociateWeeklyReport>> map = new HashMap<>();
+
+		for (AssociateWeeklyReport r : getAll()) {
+			Integer id = r.getAssId();
+			if (!map.containsKey(id)) {
+				List<AssociateWeeklyReport> newList = new ArrayList<>();
+				newList.add(r);
+				map.put(id, newList);
+			} else {
+				map.get(id).add(r);
+			}
+		}
+
+		for (List<AssociateWeeklyReport> i : map.values()) {
+			if (hasTooManyReds(i)) result.addAll(i);
+		}
+		
+		return result;
+	}
+
+	private boolean hasTooManyReds(List<AssociateWeeklyReport> reports) {
+		int redCount = 0;
+		for (AssociateWeeklyReport r : reports) {
+			if (r.isRed())
+				redCount++;
+		}
+		float redDensity = ((float) redCount) / ((float) reports.size());
+		return redDensity >= .4;
 	}
 }
