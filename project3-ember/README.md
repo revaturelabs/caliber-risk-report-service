97cf841a8f1e55c38df4201db58d99dde36600f6
Alexanderlewiss <ahs282@gmail.com>
2018-09-13 01:05:14 -0500

    Ember Documentation

--- project3-ember/README.md
+++
@@ -1,2 +0,0 @@
-# caliber-risk-report-service
-An Ember.js/Spring Boot/Cassandra microservice delivering risk analysis reporting.
--- project3-ember/app/adapters/application.js
+++
@@ -1,5 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.RESTAdapter.extend({
-    host: 'http://18.220.50.95:9090/api/staging-caliber-risk-report'
-})
